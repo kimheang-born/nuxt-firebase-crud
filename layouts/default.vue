a9bd08794b31a87fdc0365129f27dbf2bd36c0db
Kimheang <kimheang.born@z1fsc.com>
2022-10-11 16:35:49 +0700

    add new components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
+        <TheHeader @sidenavToggle="displaySidenav = !displaySidenav"/>
+        <TheSidenav 
+            :show="displaySidenav"
+            @close="displaySidenav = false"
+        />
         <nuxt />
     </div>
 </template>
 
 <script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+import TheSideNavToggle from "@/components/Navigation/TheSideNavToggle";
 export default {
-    
+    components: {
+        TheHeader,
+        TheSidenav,
+        TheSideNavToggle
+    },
+    data() {
+        return {
+            displaySidenav: false,
+        }
+    },
 }
 </script>
 
